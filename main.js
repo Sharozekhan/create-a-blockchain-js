5a457280d881c44ea86da3b6436d2168dc248df8
sharozekhan <sharoze.khan@hotmail.co.uk>
2022-06-04 20:23:58 +0100

    added mining rewards and transactions, bug in mining rewards - not granting

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;   
+    }
+}
+
 class Block{
-    constructor(index, timestamp, data, previousHash = ''){
-        this.index = index;
+    constructor( timestamp, transactions, previousHash = ''){
         this.timestamp = timestamp;
-        this.data = data; 
+        this.transactions = transactions; 
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
 
     mineBlock(difficulty){
@@ -28,21 +35,51 @@ class Block{
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 4;
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
-        return new Block(0, "01/01/2017", "Gensis Block", "0");
+        return new Block("01/01/2017", "Gensis Block", "0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log('Block mined succesfully!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTranscation(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+    
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        //A loop to check each block in the chain 
+        for (const block of this.chain){
+                for (const trans of block.transactions){
+                    if(trans.fromAddress === address){
+                       balance -= trans.amount;
+                    }
+
+                    if(trans.toAddress === address){
+                       balance += trans.amount;
+                    }
+                }
+        }
+        return balance;
     }
 
     isChainValid(){
@@ -64,8 +101,15 @@ class Blockchain{
 }
 
 let savjeeCoin = new Blockchain();
-console.log("Mining block 1...");
-savjeeCoin.addBlock(new Block(1, "10/07/2017", {amount: 4}));
+savjeeCoin.createTranscation(new Transaction('address1','address2', 100));
+savjeeCoin.createTranscation(new Transaction('address2','address1', 50));
 
-console.log("Mining block 2...");
-savjeeCoin.addBlock(new Block(2, "12/07/2017", {amount: 10}));
+console.log('\n starting the miner...');
+savjeeCoin.minePendingTransactions('sharoze-address');
+
+console.log('\nBalance of Sharoze is:', savjeeCoin.getBalanceOfAddress('sharoze-addresss'));
+
+console.log('\n starting the miner again...');
+savjeeCoin.minePendingTransactions('sharoze-address');
+
+console.log('\nBalance of Sharoze is:', savjeeCoin.getBalanceOfAddress('sharoze-addresss'));
